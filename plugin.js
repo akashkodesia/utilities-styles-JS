dda337dac8ddf411bba661d5e8933bf2a6e0de19
Akash Kodesia <akashkodesia08@gmail.com>
2019-04-24 23:43:51 +0530

    Added custom selector options and fixed code bugs

--- plugin.js
+++ plugin.js
@@ -4,130 +4,123 @@
         md = 992,
         lg = 1200,
         unit = 'px',
-        selector = 'akj-'
+        selector = 'e-',
+        elements = document.querySelectorAll('[class*=' + selector + ']'),
+        vewportWidth = window.innerWidth,
+        selectorLenght = selector.split('').length;
 
-    var elements = document.querySelectorAll('[class*=' + selector + ']');
-    console.log(elements);
-    var vewportWidth = window.innerWidth;
+    /* All padding Cases All */
+    function padding(cases) {
+        switch (cases) {
+            case 'p-':
+                return elements[i].style.padding = value + unit;
+            case 'pt-':
+                return elements[i].style.paddingTop = value + unit;
+            case 'pb-':
+                return elements[i].style.paddingBottom = value + unit;               
+            case 'pl-':
+                return elements[i].style.paddingLeft = value + unit;               
+            case 'pr-':
+                return elements[i].style.paddingRight = value + unit;               
+        }
+    }
+
+    function paddingXS(cases) {
+        switch (cases) {
+            case 'p-xs-':
+                return elements[i].style.padding = value + unit;               
+            case 'pt-xs-':
+                return elements[i].style.paddingTop = value + unit;
+            case 'pb-xs-':
+                return elements[i].style.paddingBottom = value + unit;               
+            case 'pl-xs-':
+                return elements[i].style.paddingLeft = value + unit;
+            case 'pr-xs-':
+                return elements[i].style.paddingRightTop = value + unit;
+               
+        }
+    }
+
+    function paddingSM(cases) {
+        switch (cases) {
+            case 'p-sm-':
+                return elements[i].style.padding = value + unit;               
+            case 'pt-sm-':
+                return elements[i].style.paddingTop = value + unit;               
+            case 'pb-sm-':
+                return elements[i].style.paddingBottom = value + unit;               
+            case 'pl-sm-':
+                return elements[i].style.paddingLeft = value + unit;               
+            case 'pr-sm-':
+                return elements[i].style.paddingRight = value + unit;
+        }
+    }
+
+    function paddingMD(cases) {
+        switch (cases) {
+            case 'p-md-':
+                return elements[i].style.padding = value + unit;               
+            case 'pt-md-':
+                return elements[i].style.paddingTop = value + unit;               
+            case 'pb-md-':
+                return elements[i].style.paddingBottom = value + unit;               
+            case 'pl-md-':
+                return elements[i].style.paddingLeft = value + unit;               
+            case 'pr-md-':
+                return elements[i].style.paddingRight = value + unit;
+               
+        }
+    }
+
+    function paddingLG(cases) {
+        switch (cases) {
+            case 'p-lg-':
+                return elements[i].style.padding = value + unit;               
+            case 'pt-lg-':
+                return elements[i].style.paddingTop = value + unit;               
+            case 'pb-lg-':
+                return elements[i].style.paddingBottom = value + unit;
+            case 'pl-lg-':
+                return elements[i].style.paddingLeft = value + unit;
+            case 'pr-lg-':
+                return elements[i].style.paddingRight = value + unit;
+               
+        }
+    }
 
     for (var i = 0; i < elements.length; i++) {
 
         var allClass = elements[i].classList;
-        console.log(allClass);
+        
         for (var j = 0; j < allClass.length; j++) {
-            console.log(allClass[j]);
-            if(allClass[j].indexOf(selector)!=-1){
-                var required = allClass[j].substr(4), /* Calc for selector*/
+        
+            if (allClass[j].indexOf(selector) != -1) {
+                var required = allClass[j].substr(selectorLenght),
                     value = required.match(/\d/g).toString().replace(",", ""),
                     cases = required.replace(/\d/g, '');
-                    debugger;
-            console.log(required);
-            console.log(cases);
-
+                padding(cases);
                 // Starting Cases for padding
                 if (/^p/.test(required)) {
-                    // Case will execute on all the screen sizes
-                    switch (cases) {
-                        case 'p-':
-                            elements[i].style.padding = value + unit;
-                            break;
-                        case 'pt-':
-                            elements[i].style.paddingTop = value + unit;
-                            break;
-                        case 'pb-':
-                            elements[i].style.paddingBottom = value + unit;
-                            break;
-                        case 'pl-':
-                            elements[i].style.paddingLeft = value + unit;
-                            break;
-                        case 'pr-':
-                            elements[i].style.paddingRightTop = value + unit;
-                            break;
-                    }
+                    
                     // Case will execute on mobile screens
-                    if (vewportWidth < sm) { debugger;
-                        switch (cases) {
-                            case 'p-xs-':
-                                elements[i].style.padding = value + unit;
-                                break;
-                            case 'pt-xs-':
-                                elements[i].style.paddingTop = value + unit;
-                                break;
-                            case 'pb-xs-':
-                                elements[i].style.paddingBottom = value + unit;
-                                break;
-                            case 'pl-xs-':
-                                elements[i].style.paddingLeft = value + unit;
-                                break;
-                            case 'pr-xs-':
-                                elements[i].style.paddingRightTop = value + unit;
-                                break;
-                        }
+                    if (vewportWidth < sm) {
+                        paddingXS(cases);
                     }
-                    
                     // Case will execute on small screens    
-                    if (vewportWidth >= sm && vewportWidth < md ) {
-                        switch (cases) {
-                            case 'p-sm-':
-                                elements[i].style.padding = value + unit;
-                                break;
-                            case 'pt-sm-':
-                                elements[i].style.paddingTop = value + unit;
-                                break;
-                            case 'pb-sm-':
-                                elements[i].style.paddingBottom = value + unit;
-                                break;
-                            case 'pl-sm-':
-                                elements[i].style.paddingLeft = value + unit;
-                                break;
-                            case 'pr-sm-':
-                                elements[i].style.paddingRightTop = value + unit;
-                                break;
-                        }
+                    else if (vewportWidth >= sm && vewportWidth < md) {
+                        paddingSM(cases);
                     }
-                    
                     // Case will execute on medium devices    
-                    if (vewportWidth >= md && vewportWidth < lg ) {
-                        switch (cases) {
-                            case 'p-md-':
-                                elements[i].style.padding = value + unit;
-                                break;
-                            case 'pt-md-':
-                                elements[i].style.paddingTop = value + unit;
-                                break;
-                            case 'pb-md-':
-                                elements[i].style.paddingBottom = value + unit;
-                                break;
-                            case 'pl-md-':
-                                elements[i].style.paddingLeft = value + unit;
-                                break;
-                            case 'pr-md-':
-                                elements[i].style.paddingRightTop = value + unit;
-                                break;
-                        }
+                    else if (vewportWidth >= md && vewportWidth < lg) {
+                        paddingMD(cases);
+                    }
+
+                    // Case will execute on large devices    
+                    else if (vewportWidth >= lg) {
+                        paddingLG(cases);
                     }
                     
-                     // Case will execute on large devices    
-                    if (vewportWidth >= lg ) {
-                        switch (cases) {
-                            case 'p-lg-':
-                                elements[i].style.padding = value + unit;
-                                break;
-                            case 'pt-lg-':
-                                elements[i].style.paddingTop = value + unit;
-                                break;
-                            case 'pb-lg-':
-                                elements[i].style.paddingBottom = value + unit;
-                                break;
-                            case 'pl-lg-':
-                                elements[i].style.paddingLeft = value + unit;
-                                break;
-                            case 'pr-lg-':
-                                elements[i].style.paddingRightTop = value + unit;
-                                break;
-                        }
-                    }
-                }                
+                }
             }
         }
 
